2dd269bec0a57701e00fcd118aeac36671e47288
ashwiniwagh2296 <ashwiniwagh2296@gmail.com>
2020-08-28 12:59:33 +0530

    Add project files.

---
+++ DataBaseProject/SignUp.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace DataBaseProject
+{
+    partial class SignUp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_SignUp = new System.Windows.Forms.Label();
+            this.lbl_Name = new System.Windows.Forms.Label();
+            this.txt_Name = new System.Windows.Forms.TextBox();
+            this.txt_Email = new System.Windows.Forms.TextBox();
+            this.lbl_Email = new System.Windows.Forms.Label();
+            this.txt_Password = new System.Windows.Forms.TextBox();
+            this.lbl_Password = new System.Windows.Forms.Label();
+            this.txt_Country = new System.Windows.Forms.ComboBox();
+            this.lbl_Country = new System.Windows.Forms.Label();
+            this.btn_SignUp = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_SignUp
+            // 
+            this.lbl_SignUp.AutoSize = true;
+            this.lbl_SignUp.Font = new System.Drawing.Font("Arial", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_SignUp.ForeColor = System.Drawing.Color.Black;
+            this.lbl_SignUp.Location = new System.Drawing.Point(73, 35);
+            this.lbl_SignUp.Name = "lbl_SignUp";
+            this.lbl_SignUp.Size = new System.Drawing.Size(108, 32);
+            this.lbl_SignUp.TabIndex = 0;
+            this.lbl_SignUp.Text = "SignUp";
+            // 
+            // lbl_Name
+            // 
+            this.lbl_Name.AutoSize = true;
+            this.lbl_Name.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Name.ForeColor = System.Drawing.Color.Black;
+            this.lbl_Name.Location = new System.Drawing.Point(76, 107);
+            this.lbl_Name.Name = "lbl_Name";
+            this.lbl_Name.Size = new System.Drawing.Size(74, 24);
+            this.lbl_Name.TabIndex = 1;
+            this.lbl_Name.Text = "Name:";
+            // 
+            // txt_Name
+            // 
+            this.txt_Name.Location = new System.Drawing.Point(198, 112);
+            this.txt_Name.Name = "txt_Name";
+            this.txt_Name.Size = new System.Drawing.Size(334, 20);
+            this.txt_Name.TabIndex = 2;
+            // 
+            // txt_Email
+            // 
+            this.txt_Email.Location = new System.Drawing.Point(198, 157);
+            this.txt_Email.Name = "txt_Email";
+            this.txt_Email.Size = new System.Drawing.Size(334, 20);
+            this.txt_Email.TabIndex = 4;
+            // 
+            // lbl_Email
+            // 
+            this.lbl_Email.AutoSize = true;
+            this.lbl_Email.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Email.ForeColor = System.Drawing.Color.Black;
+            this.lbl_Email.Location = new System.Drawing.Point(76, 153);
+            this.lbl_Email.Name = "lbl_Email";
+            this.lbl_Email.Size = new System.Drawing.Size(72, 24);
+            this.lbl_Email.TabIndex = 3;
+            this.lbl_Email.Text = "Email:";
+            // 
+            // txt_Password
+            // 
+            this.txt_Password.Location = new System.Drawing.Point(198, 198);
+            this.txt_Password.Name = "txt_Password";
+            this.txt_Password.Size = new System.Drawing.Size(334, 20);
+            this.txt_Password.TabIndex = 6;
+            // 
+            // lbl_Password
+            // 
+            this.lbl_Password.AutoSize = true;
+            this.lbl_Password.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Password.ForeColor = System.Drawing.Color.Black;
+            this.lbl_Password.Location = new System.Drawing.Point(76, 194);
+            this.lbl_Password.Name = "lbl_Password";
+            this.lbl_Password.Size = new System.Drawing.Size(116, 24);
+            this.lbl_Password.TabIndex = 5;
+            this.lbl_Password.Text = "Password:";
+            // 
+            // txt_Country
+            // 
+            this.txt_Country.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Country.FormattingEnabled = true;
+            this.txt_Country.Items.AddRange(new object[] {
+            "Afghanistan",
+            "Albania",
+            "Algeria",
+            "Belgium",
+            "Belize",
+            "China",
+            "Colombia",
+            "Comoros",
+            "France",
+            "Iceland",
+            "India",
+            "Indonesia",
+            "Iran",
+            "Iraq",
+            "Ireland",
+            "Israel",
+            "Italy",
+            "Japan",
+            "Spain",
+            "Sri Lanka"});
+            this.txt_Country.Location = new System.Drawing.Point(198, 249);
+            this.txt_Country.Name = "txt_Country";
+            this.txt_Country.Size = new System.Drawing.Size(334, 32);
+            this.txt_Country.TabIndex = 7;
+            // 
+            // lbl_Country
+            // 
+            this.lbl_Country.AutoSize = true;
+            this.lbl_Country.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Country.ForeColor = System.Drawing.Color.Black;
+            this.lbl_Country.Location = new System.Drawing.Point(76, 246);
+            this.lbl_Country.Name = "lbl_Country";
+            this.lbl_Country.Size = new System.Drawing.Size(97, 24);
+            this.lbl_Country.TabIndex = 8;
+            this.lbl_Country.Text = "Country:";
+            // 
+            // btn_SignUp
+            // 
+            this.btn_SignUp.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_SignUp.ForeColor = System.Drawing.Color.Black;
+            this.btn_SignUp.Location = new System.Drawing.Point(437, 314);
+            this.btn_SignUp.Name = "btn_SignUp";
+            this.btn_SignUp.Size = new System.Drawing.Size(95, 41);
+            this.btn_SignUp.TabIndex = 9;
+            this.btn_SignUp.Text = "SignUp";
+            this.btn_SignUp.UseVisualStyleBackColor = true;
+            this.btn_SignUp.Click += new System.EventHandler(this.btn_SignUp_Click);
+            // 
+            // SignUp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_SignUp);
+            this.Controls.Add(this.lbl_Country);
+            this.Controls.Add(this.txt_Country);
+            this.Controls.Add(this.txt_Password);
+            this.Controls.Add(this.lbl_Password);
+            this.Controls.Add(this.txt_Email);
+            this.Controls.Add(this.lbl_Email);
+            this.Controls.Add(this.txt_Name);
+            this.Controls.Add(this.lbl_Name);
+            this.Controls.Add(this.lbl_SignUp);
+            this.Name = "SignUp";
+            this.Text = "SignUp";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_SignUp;
+        private System.Windows.Forms.Label lbl_Name;
+        private System.Windows.Forms.TextBox txt_Name;
+        private System.Windows.Forms.TextBox txt_Email;
+        private System.Windows.Forms.Label lbl_Email;
+        private System.Windows.Forms.TextBox txt_Password;
+        private System.Windows.Forms.Label lbl_Password;
+        private System.Windows.Forms.ComboBox txt_Country;
+        private System.Windows.Forms.Label lbl_Country;
+        private System.Windows.Forms.Button btn_SignUp;
+    }
+}
+
---
+++ DataBaseProject/SignUp.cs
@@ -0,0 +1,76 @@
+﻿using DatabaseProject;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace DataBaseProject
+{
+    public partial class SignUp : Form
+    {
+        dbAccess ObjDbAccess = new dbAccess();
+        public SignUp()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_SignUp_Click(object sender, EventArgs e)
+        {
+            string UserName = txt_Name.Text;
+            string UserEmail = txt_Email.Text;
+            string UserPassword = txt_Password.Text;
+            string UserCountry = txt_Country.Text;
+
+            if (UserName.Equals(" "))
+            {
+                MessageBox.Show("Please enter Your user name.");
+            }
+            else if (UserEmail.Equals(" "))
+            {
+                MessageBox.Show("Please enter Your  Email.");
+            }
+            else if (UserPassword.Equals(" "))
+            {
+                MessageBox.Show("Please enter Your Password.");
+            }
+            else if (UserCountry.Equals(" "))
+            {
+                MessageBox.Show("Please enter Your Country.");
+            }
+            else
+            {
+                SqlCommand insertCommand = new SqlCommand("Insert into Users(Name,Email,Password,Country) Values(@UserName,@UserEmail,@UserPassword,@UserCountry)");
+
+                insertCommand.Parameters.AddWithValue("@userName", UserName);
+                insertCommand.Parameters.AddWithValue("@userEmail", UserEmail);
+                insertCommand.Parameters.AddWithValue("@userPassword", UserPassword);
+                insertCommand.Parameters.AddWithValue("@userCountry", UserCountry);
+
+                int row = ObjDbAccess.executeQuery(insertCommand);
+                if (row == 1)
+                {
+                    MessageBox.Show("Account created successfully");
+                    this.Hide();
+
+                    HomePage home = new HomePage();
+                    home.Show();
+
+                }
+                else
+                {
+                    MessageBox.Show("Error Occured..Try Again");
+                }
+            }
+        }
+    }
+}
+
+        
+
